766585af1a7077200982e7f9914f0ed565dd752a
9autam-chavda <9autamchavda@gmail.com>
2026-08-01 01:34:02 +0530

    Replace SMTP with Brevo email service

--- server/src/services/email.service.js
+++ server/src/services/email.service.js
@@ -1,64 +1,74 @@
-const { Resend } = require("resend");
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+const brevo = require("@getbrevo/brevo");
 
 class EmailService {
+  constructor() {
+    this.api = new brevo.TransactionalEmailsApi();
+
+    this.api.setApiKey(
+      brevo.TransactionalEmailsApiApiKeys.apiKey,
+      process.env.BREVO_API_KEY
+    );
+  }
+
   async sendMail({ to, subject, html }) {
     try {
-      console.log("======= SEND EMAIL =======");
-      console.log("TO:", to);
-
-      const response = await resend.emails.send({
-        from: process.env.EMAIL_FROM,
-        to,
+      await this.api.sendTransacEmail({
+        sender: {
+          name: "MemoryOS",
+          email: "memoryos.otp@gmail.com",
+        },
+        to: [
+          {
+            email: to,
+          },
+        ],
         subject,
-        html,
+        htmlContent: html,
       });
 
-      console.log("EMAIL SENT");
-      console.log(response);
-
-      return response;
+      console.log("Email sent:", to);
     } catch (error) {
-      console.error("EMAIL FAILED");
-      console.error(error);
+      console.error(
+        "BREVO ERROR:",
+        error.response?.body || error
+      );
 
       throw new Error("Unable to send email.");
     }
   }
 
   async sendVerificationOTP(email, otp) {
-    return this.sendMail({
+    await this.sendMail({
       to: email,
       subject: "Verify your MemoryOS account",
       html: `
         <h2>Welcome to MemoryOS</h2>
 
-        <p>Your verification code is:</p>
+        <p>Your verification code is</p>
 
-        <h1 style="letter-spacing:6px;">${otp}</h1>
+        <h1 style="letter-spacing:6px">
+          ${otp}
+        </h1>
 
         <p>This code expires in <b>10 minutes</b>.</p>
-
-        <p>If you didn't create this account, you can safely ignore this email.</p>
       `,
     });
   }
 
   async sendResetPasswordOTP(email, otp) {
-    return this.sendMail({
+    await this.sendMail({
       to: email,
       subject: "Reset your MemoryOS password",
       html: `
         <h2>Reset Password</h2>
 
-        <p>Your password reset code is:</p>
+        <p>Your OTP is</p>
 
-        <h1 style="letter-spacing:6px;">${otp}</h1>
+        <h1 style="letter-spacing:6px">
+          ${otp}
+        </h1>
 
         <p>This code expires in <b>10 minutes</b>.</p>
-
-        <p>If you didn't request this, please ignore this email.</p>
       `,
     });
   }
